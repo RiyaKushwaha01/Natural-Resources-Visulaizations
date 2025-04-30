4f7beb0031e10553c57a75be13b1c5ece9eb767c
RiyaKushwaha01 <riyakushwaha1125@gmail.com>
2025-04-30 22:40:17 +0530

    Update app.py

--- app.py
+++ app.py
@@ -71,6 +71,20 @@ else:
         "Fuel Oil", "Helium"
     ])
 
+    # Apply all filters
+    filtered_df = df[
+        (df["Calendar Year"].isin(selected_years)) &
+        (df["Land Class"].isin(selected_land_classes)) &
+        (df["State"].isin(selected_states)) &
+        (df["Land Category"].isin(selected_land_categories)) &
+        (df["State"] == state) &
+        (df["Revenue Type"] == revenue_type) &
+        (df["Mineral Lease Type"] == lease_type) &
+        (df["Commodity"] == commodity) &
+        (df["County"] == county) &
+        (df["Product"] == product)
+    ]
+
     # Dataset preview
     st.subheader("Dataset Preview")
     st.dataframe(filtered_df.head())
